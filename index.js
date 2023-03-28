9ce75b7aa7345da9c928d9a2b208eca4bd69af4a
dvorko <100367460+dvorko@users.noreply.github.com>
2023-03-28 17:04:55 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const mainContainer = document.querySelector(".main-container");
+const thanksContainer = document.querySelector(".thank-you");
+const submitButton = document.getElementById("submit");
+const rateAgainButton = document.getElementById("rate-again");
+const rating = document.getElementById("rating");
+const rates = document.querySelectorAll(".btn");
+
+submitButton.addEventListener("click", () => {
+  thanksContainer.classList.remove("hidden");
+  mainContainer.style.display = "none";
+});
+rateAgainButton.addEventListener("click", () => {
+  thanksContainer.classList.add("hidden");
+  mainContainer.style.display = "block";
+});
+
+rates.forEach((rate) => {
+  rate.addEventListener("click", () => {
+    rating.innerHTML = rate.innerHTML; 
+  });
+});
